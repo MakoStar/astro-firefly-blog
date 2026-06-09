05223086e1c934e674c60829b0baba21890b044a
MakoStar <makostar.github@gmail.com>
2026-06-10 01:37:40 +0800

    change: 部署站点地址

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,13 +7,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "MakoStar",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://makostar.github.io",
 
 	// 站点描述
 	description:
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"MakoStar",
 		"Fuwari",
 		"Astro",
 		"ACGN",
